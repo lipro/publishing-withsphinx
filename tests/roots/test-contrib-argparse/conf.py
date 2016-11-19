17035ead8a4743dd1a6d44a90f92fab9863ea7b4
Stephan Linz <linz@li-pro.net>
2016-11-19 22:22:58 +0100

    tests: split off sphinxarg.ext
    releated to issue #2
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ tests/roots/test-contrib-argparse/conf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('samples'))
+
+needs_sphinx = '1.2'
+# FIXME: needs_extensions = {'sphinxarg.ext': '0.1'}
+extensions = ['publishing.withsphinx']
+master_doc = 'index'
+latex_documents = [(master_doc, 'index.tex', 'project', 'author', 'manual')]
---
+++ tests/roots/test-contrib-argparse/main.rst
@@ -0,0 +1,14 @@
+.. -*- coding: utf-8 -*-
+.. -*- restructuredtext -*-
+
+Fancytool command
+=================
+
+.. argparse::
+   :module: cmdargs
+   :func: parser
+   :prog: cmdargs
+
+subcommand
+  Here we add reference to subcommand, to simplify navigation.
+  See :doc:`subcommand`
