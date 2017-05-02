3eafb51e994fe46cfb8deb8966b0c8079f25c448
Stephan Linz <linz@li-pro.net>
2017-05-02 19:09:23 +0200

    setuptools: bump sphinxcontrib-programoutput to 0.10.0
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- tests/roots/conf.py
+++ tests/roots/conf.py
@@ -27,7 +27,7 @@ needs_extensions = {
 #   'sphinxcontrib.email':                      '0.3',  # NOQA
 #   'sphinxcontrib.embedly':                    '0.2',  # NOQA
 #   'sphinxcontrib.inlinesyntaxhighlight':      '0.2',  # NOQA
-#   'sphinxcontrib.programoutput':              '0.8',  # NOQA
+#   'sphinxcontrib.programoutput':              '0.10',  # NOQA
 #   'sphinxcontrib.spelling':                   '2.2',  # NOQA
 #   'sphinxcontrib.tikz':                       '0.4',  # NOQA
 }
