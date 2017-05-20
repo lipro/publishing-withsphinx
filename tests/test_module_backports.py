165070ec2fcaf04574be4f09a446b6aff9f45464
Stephan Linz <linz@li-pro.net>
2017-05-20 16:26:24 +0200

    tests: add unit tests for backports
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- tests/test_module_backports.py
+++ tests/test_module_backports.py
@@ -28,8 +28,8 @@
 test_module_backports
 ~~~~~~~~~~~~~~~~~~~~~
 
-This module contains basic functional tests over all supported extensions
-as part of the publishing.withsphinx package to evaluate all backports.
+This module contains basic unit and functional tests over all supported
+backports as part of the publishing.withsphinx package.
 
 :copyright: Copyright 2014-2017 by Li-Pro.Net, see AUTHORS.
 :license: MIT, see LICENSE for details.
@@ -39,15 +39,98 @@ from __future__ import absolute_import
 
 from tests import util
 
-from nose.tools import raises
 from sphinx.errors import SphinxError
 
 _EXPECT_LATEX_ENGINE_DEFAULT_LANG_NONE = 'pdflatex'
 _EXPECT_LATEX_ENGINE_DEFAULT_LANG_JA = 'platex'
 _EXPECT_LATEX_ENGINE_CONFOVERRIDES = 'xelatex'
+_EXPECT_LATEX_ENGINE_VALIDS = [
+    _EXPECT_LATEX_ENGINE_DEFAULT_LANG_NONE,
+    _EXPECT_LATEX_ENGINE_DEFAULT_LANG_JA,
+    _EXPECT_LATEX_ENGINE_CONFOVERRIDES,
+    'lualatex',
+]
 
 
 class TestPublishingWithSphinxBackports(util.TestCasePublishingSphinx):
+
+    def test_package_has_return_latex_engine_defaults(self):
+        '''
+        UNIT TEST: package has return 'latex_engine' default
+        '''
+        import publishing.withsphinx.backports as pwsb
+        self.assertTrue(callable(pwsb.default_latex_engine))
+        cfg = util.mock.MagicMock()
+        self.assertEqual(pwsb.default_latex_engine(cfg), 'pdflatex')
+
+    def test_package_has_return_latex_engine_defaults_lang_ja(self):
+        '''
+        UNIT TEST: package has return 'latex_engine' with 'language' set to 'ja'
+        '''
+        import publishing.withsphinx.backports as pwsb
+        self.assertTrue(callable(pwsb.default_latex_engine))
+        cfg = util.mock.MagicMock()
+        cfg.language = 'ja'
+        self.assertEqual(pwsb.default_latex_engine(cfg), 'platex')
+
+    def test_package_has_check_latex_engine_with_valid_content(self):
+        '''
+        UNIT TEST: package has check 'latex_engine' with valid content
+        '''
+        import publishing.withsphinx.backports as pwsb
+        self.assertTrue(callable(pwsb.check_latex_engine))
+        app = util.mock.MagicMock()
+        app.config.values = dict(latex_engine=None)
+        for latex_engine in _EXPECT_LATEX_ENGINE_VALIDS:
+            app.config.latex_engine = latex_engine
+            pwsb.check_latex_engine(app)
+
+    @util.nose.tools.raises(SphinxError)
+    def test_package_has_check_latex_engine_with_invalid_content(self):
+        '''
+        UNIT TEST: package has check 'latex_engine' with invalid content
+        '''
+        import publishing.withsphinx.backports as pwsb
+        self.assertTrue(callable(pwsb.check_latex_engine))
+        app = util.mock.MagicMock()
+        app.config.values = dict(latex_engine=None)
+        app.config.latex_engine = 'nonsens'
+        pwsb.check_latex_engine(app)
+
+    @util.nose.tools.raises(SphinxError)
+    def test_package_has_check_latex_engine_not_in_config_values(self):
+        '''
+        UNIT TEST: package has check 'latex_engine' not in config values
+        '''
+        import publishing.withsphinx.backports as pwsb
+        self.assertTrue(callable(pwsb.check_latex_engine))
+        app = util.mock.MagicMock()
+        app.config.values = dict(nothing=None)
+        pwsb.check_latex_engine(app)
+
+    def test_package_has_latex_engine_add_when_not_in_config_values(self):
+        '''
+        UNIT TEST: package has add 'latex_engine' when not in config values
+        '''
+        import publishing.withsphinx.backports as pwsb
+        self.assertTrue(callable(pwsb.sphinx15))
+        app = util.mock.MagicMock()
+        app.config.values = dict(nothing=None)
+        pwsb.sphinx15(app)
+        app.add_config_value.assert_called_once_with('latex_engine', pwsb.default_latex_engine, None)
+        app.connect.assert_called_once_with('builder-inited', pwsb.check_latex_engine)
+
+    def test_package_has_latex_engine_add_when_is_in_config_values(self):
+        '''
+        UNIT TEST: package has add 'latex_engine' when is in config values
+        '''
+        import publishing.withsphinx.backports as pwsb
+        self.assertTrue(callable(pwsb.sphinx15))
+        app = util.mock.MagicMock()
+        app.config.values = dict(latex_engine=None)
+        pwsb.sphinx15(app)
+        app.add_config_value.assert_not_called()
+        app.connect.assert_not_called()
 
     @util.with_coverage_app(
         testroot='module-backports',
@@ -85,7 +168,7 @@ class TestPublishingWithSphinxBackports(util.TestCasePublishingSphinx):
         self.assertTrue(isinstance(app.config.latex_engine, str))
         self.assertEqual(app.config.latex_engine, _EXPECT_LATEX_ENGINE_CONFOVERRIDES)
 
-    @raises(SphinxError)
+    @util.nose.tools.raises(SphinxError)
     @util.with_coverage_app(
         testroot='module-backports',
         confoverrides={
