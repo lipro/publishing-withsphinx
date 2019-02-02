90cc7faf8f51e2d41e75db2df6203ebf5bcb3998
Stephan Linz <linz@li-pro.net>
2019-02-02 14:02:20 +0100

    setuptools: bump sphinx-testing to 1.0.0
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ _EXTRAS_REQUIRE_DEV = [
 ]
 
 _EXTRAS_REQUIRE_TEST = [
-    'sphinx-testing>=0.7.1,<=0.7.1',
+    'sphinx-testing>=1.0.0,<=1.0.0',
     'tox>=2.0.0,<=2.7.0',
     'nose>=1.3.7,<=1.3.7',
     'mock>=2.0.0,<=2.0.0',
